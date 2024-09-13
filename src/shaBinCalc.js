8e484cca205ea9efe782ec781628f4621d04f343
Musooo <nicoloblack2@gmail.com>
2024-09-13 22:19:30 +0200

    temp1 calc

--- src/shaBinCalc.js
+++ src/shaBinCalc.js
@@ -15,3 +15,7 @@ function getSum(str, n){
     const num =[[2,13,22],[6,11,25]];
     return (((parseInt(rightrotate(str,num[n][0],32),2))^(parseInt(rightrotate(str,num[n][1],32),2))^(parseInt(rightrotate(str,num[n][2],32),2)))>>>0).toString(2).padStart(32,"0");
 }
+
+function calcTmp1(h,summand1,choice,ki,wi){
+    return (parseInt(h,2)+parseInt(summand1,2)+parseInt(choice,2)+parseInt(ki,2)+parseInt(wi,2)).toString(2).padStart(32,"0");  
+}
