abdcd3cf4f68c9482fd57e6a7ae0e1c434b0ba3f
Musooo <nicoloblack2@gmail.com>
2024-09-13 22:18:44 +0200

    choice and summand implemented

---
+++ src/shaBinCalc.js
@@ -0,0 +1,17 @@
+
+function notBin(str){
+    let inv = '';
+    for (let char of str){
+        inv += char === '0' ? '1' : '0';
+    }
+    return inv.padStart(32,"0");
+}
+
+function getChoice(e,f,g){
+    return ((parseInt(e,2)&parseInt(f,2))^(parseInt(notBin(e),2)&parseInt(g,2))).toString(2).padStart(32,"0");
+}
+
+function getSum(str, n){
+    const num =[[2,13,22],[6,11,25]];
+    return (((parseInt(rightrotate(str,num[n][0],32),2))^(parseInt(rightrotate(str,num[n][1],32),2))^(parseInt(rightrotate(str,num[n][2],32),2)))>>>0).toString(2).padStart(32,"0");
+}
